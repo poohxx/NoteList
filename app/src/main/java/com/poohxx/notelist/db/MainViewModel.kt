c7edbdec4708714241cc4a421036a23d0e5624df
Ayrat <poohxx84@gmail.com>
2021-12-30 05:23:58 +0300

    MVVM

---
+++ app/src/main/java/com/poohxx/notelist/db/MainViewModel.kt
@@ -0,0 +1,26 @@
+package com.poohxx.notelist.db
+
+import androidx.lifecycle.*
+import com.poohxx.notelist.entities.NoteItem
+import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
+
+class MainViewModel(dataBase: MainDataBase) : ViewModel() {
+    val dao = dataBase.getDao()
+    val allNotes: LiveData<List<NoteItem>> = dao.getAllNotes().asLiveData()
+    fun insertNote(note: NoteItem) = viewModelScope.launch {
+        dao.insertNote(note)
+    }
+
+    class MainViewModelFactory(val dataBase: MainDataBase) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return MainViewModel(dataBase) as T
+            }
+            throw IllegalArgumentException("Unknow ViewModelClass")
+        }
+
+    }
+
+}
--- app/src/main/java/com/poohxx/notelist/fragments/NoteFragment.kt
+++ app/src/main/java/com/poohxx/notelist/fragments/NoteFragment.kt
@@ -5,12 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.poohxx.notelist.R
+import com.poohxx.notelist.activities.MainApp
 import com.poohxx.notelist.databinding.FragmentNoteBinding
+import com.poohxx.notelist.db.MainViewModel
 
 class NoteFragment : BaseFragment() {
     private lateinit var binding: FragmentNoteBinding
+    private val mainViewModel: MainViewModel by activityViewModels {
+        MainViewModel.MainViewModelFactory((context?.applicationContext as MainApp).dataBase)
+    }
+
     override fun onClickNew() {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+     
     }
 
     override fun onCreateView(
