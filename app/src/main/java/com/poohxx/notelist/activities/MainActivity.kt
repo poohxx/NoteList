af779df3a4aefa00cde6f2a605f7ac57d8d72dd7
Ayrat <poohxx84@gmail.com>
2022-02-04 10:48:27 +0300

    add Ads in settings

--- app/src/main/java/com/poohxx/notelist/activities/MainActivity.kt
+++ app/src/main/java/com/poohxx/notelist/activities/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.poohxx.notelist.activities
 
 import TaskListNamesFragment
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.poohxx.notelist.R
 import com.poohxx.notelist.databinding.ActivityMainBinding
 import com.poohxx.notelist.dialogs.NewListDialog
@@ -14,11 +22,17 @@ import com.poohxx.notelist.fragments.FragmentManager
 import com.poohxx.notelist.fragments.NoteFragment
 import com.poohxx.notelist.settings.SettingsActivity
 
+
 class MainActivity : AppCompatActivity(), NewListDialog.Listener {
     lateinit var binding: ActivityMainBinding
     private var currentMenuItemId = R.id.task_list
     private lateinit var defPref: SharedPreferences
     private var currentTheme = ""
+    private var iAd: InterstitialAd? = null
+    private var counterAD = 0
+    private var counterAdMax = 4
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         defPref = PreferenceManager.getDefaultSharedPreferences(this)
         setTheme(getSelectedTheme())
@@ -26,6 +40,7 @@ class MainActivity : AppCompatActivity(), NewListDialog.Listener {
         binding = ActivityMainBinding.inflate(layoutInflater)
         currentTheme = defPref.getString("choose_theme_key", "Yellow").toString()
         setContentView(binding.root)
+
         FragmentManager.setFragment(
             TaskListNamesFragment.newInstance(),
             this
@@ -33,11 +48,62 @@ class MainActivity : AppCompatActivity(), NewListDialog.Listener {
         setBotNavViewListener()
     }
 
+    private fun loadInterAd() {
+        val adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(
+            this,
+            getString(R.string.inter_ad_id),
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdLoaded(ad: InterstitialAd) {
+                    iAd = ad
+                }
+
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    iAd = null
+                }
+
+            })
+    }
+
+    fun showInterAd(adListener: AdListener) {
+        if (iAd != null&& counterAD>counterAdMax) {
+            iAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+                override fun onAdDismissedFullScreenContent() {
+                    iAd = null
+                    loadInterAd()
+                    adListener.onFinish()
+                }
+
+                override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+                    iAd = null
+                    loadInterAd()
+                }
+
+                override fun onAdShowedFullScreenContent() {
+                    iAd = null
+                    loadInterAd()
+                }
+            }
+            counterAD=0
+            iAd?.show(this)
+        } else {
+            counterAD++
+            adListener.onFinish()
+        }
+    }
+
     private fun setBotNavViewListener() {
         binding.btmNavView.setOnItemReselectedListener {
             when (it.itemId) {
                 R.id.settings -> {
-                    startActivity(Intent(this, SettingsActivity::class.java))
+                    showInterAd(object : AdListener {
+                        override fun onFinish() {
+                            startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
+                        }
+                    })
+                    Toast.makeText(this,"It's working",Toast.LENGTH_LONG).show()
                 }
                 R.id.new_task -> {
                     FragmentManager.currentFrag?.onClickNew()
@@ -51,11 +117,14 @@ class MainActivity : AppCompatActivity(), NewListDialog.Listener {
                     )
                 }
                 R.id.notes -> {
-                    currentMenuItemId = R.id.notes
-                    FragmentManager.setFragment(
-                        NoteFragment.newInstance(),
-                        this
-                    )
+                    showInterAd(object : AdListener {
+                        override fun onFinish() {
+                            currentMenuItemId = R.id.notes
+                            FragmentManager.setFragment(
+                                NoteFragment.newInstance(),this@MainActivity)
+                        }
+                    })
+
                 }
             }
             true
@@ -80,5 +149,9 @@ class MainActivity : AppCompatActivity(), NewListDialog.Listener {
 
     override fun onClick(name: String) {
         Toast.makeText(this, "Name: $name", Toast.LENGTH_SHORT).show()
+    }
+
+    interface AdListener {
+        fun onFinish()
     }
 }
